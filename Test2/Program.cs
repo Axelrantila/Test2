fd91516ab00f4105112530944ffd471f42239e9e
Axel Räntilä <axelrantila@gmail.com>
2016-04-29 15:49:21 +0200

    Changed goodbye message

--- Test2/Program.cs
+++ Test2/Program.cs
@@ -11,7 +11,7 @@ namespace Test2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("And Goodbye!");
+            Console.WriteLine("And Goodbye! No refunds.");
         }
     }
 }
